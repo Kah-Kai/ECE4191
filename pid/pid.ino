89e57d9ab791d69592626b6653622d5f45992d35
RileyCheung <126927147+RileyCheung@users.noreply.github.com>
2024-09-17 23:48:27 +1000

    Add files via upload
    update pid files

--- pid/pid.ino
+++ pid/pid.ino
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#include "pidClass.h"
+#include "PIDClass.h"
 #define ENCODER_LEFT 2
 #define ENCODER_RIGHT 7
 #define ENCODER_CON 10
@@ -8,9 +8,9 @@
 String data_string = "";  
 
 // Motor objects for LeftMotor, RightMotor, and converyer belt
-pidClass leftMotor = pidClass(3, 4, 2, 12*74.83);    // Pins for LeftMotor: in1 = 3, in2 = 4, encoder_pin = 2, encoder_ticks_per_rev = 1200
-pidClass RightMotor = pidClass(5, 6, 7, 12*74.83);   // Pins for RightMotor: in1 = 5, in2 = 6, encoder_pin = 7, encoder_ticks_per_rev = 1200
-pidClass conMotor = pidClass(8, 9, 10, 12*74.83);   // Pins for MiddleMotor: in1 = 8, in2 = 9, encoder_pin = 10, encoder_ticks_per_rev = 1200
+PIDClass leftMotor = PIDClass(3, 4, 2, 12*74.83);    // Pins for LeftMotor: in1 = 3, in2 = 4, encoder_pin = 2, encoder_ticks_per_rev = 1200
+PIDClass rightMotor = PIDClass(5, 6, 7, 12*74.83);   // Pins for RightMotor: in1 = 5, in2 = 6, encoder_pin = 7, encoder_ticks_per_rev = 1200
+PIDClass conMotor = PIDClass(8, 9, 10, 12*74.83);   // Pins for MiddleMotor: in1 = 8, in2 = 9, encoder_pin = 10, encoder_ticks_per_rev = 1200
 
 // setup interrupts
 void left_encoder_isr(){
@@ -53,27 +53,4 @@ void loop() {
 
 // Function to parse and execute commands for motors
 void parseAndExecuteCommand(String command) {
-  char motor = command.charAt(0);  // Get motor identifier (L, R, M)
-  int speed = command.substring(1).toInt();  // Get the speed (after the motor identifier)
-
-  // Control the correct motor based on the input
-  if (motor == 'L') {
-    LeftMotor.control(speed);  // Control LeftMotor
-    Serial.print("LeftMotor set to: ");
-  } 
-  else if (motor == 'R') {
-    RightMotor.control(speed);  // Control RightMotor
-    Serial.print("RightMotor set to: ");
-  } 
-  else if (motor == 'M') {
-    MiddleMotor.control(speed);  // Control MiddleMotor
-    Serial.print("MiddleMotor set to: ");
-  }
-  else {
-    Serial.println("Invalid motor identifier.");
-    return;
-  }
-
-  // Print the received command and speed
-  Serial.println(speed);
 }
