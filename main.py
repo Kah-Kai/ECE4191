30ff592faf42715b565aae3ca55fbb1b0cf7f213
Kah Kai <kahkaikhoo@gmail.com>
2024-09-20 13:22:03 +1000

    Added main.py

--- main.py
+++ main.py
@@ -3,25 +3,40 @@ import numpy as np
 import cv2
 
 # Import classes
-from DiffDriveRobot import DiffDriveRobot, Controller
+from detector import Detector
 
+
+
+# TODO: Update values in the params folder
 # Load parameters from callibration
-wheel_radius = 0
-wheel_sep = 0
-camera_matrix = np.array()
+wheel_radius = np.loadtxt("params/wheel_radius.txt")
+wheel_sep = np.loadtxt("params/wheel_sep.txt")
+
+
+capacity = 3   # Ball capacity of robot
+num_balls = 0  # Number of balls collected
 
 # Initialize classes
-bot = DiffDriveRobot(wheel_radius=wheel_radius, wheel_sep=wheel_sep,)
+detector = Detector() # Initialise detector
+
+def ball_search():
+    pass
+
+def align_with_ball():
+    pass
+
+def approach_ball():
+    pass
 
 
 # Infinite loop
-while True:
-    # While number of balls on robot < capacity:
-        # Detect balls and save locations
-        # Plan path
-        # Navigate to ball and collect
-    # Drive to box
-    # Deposit balls 
-    pass
+# while True:
+#     # While number of balls on robot < capacity:
+#         # Detect balls
+#         # Navigate to ball and collect
+#     # Drive to box
+#     # Deposit balls 
+#     pass
+
 
 
